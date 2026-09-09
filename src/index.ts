ea4f6cdd4bc3678c7264f0f5f03756538d4362f2
Martin Pokorny <martin.pokorny@etm.at>
2026-09-09 17:13:24 +0200

    format

--- src/index.ts
+++ src/index.ts
@@ -162,9 +162,9 @@ export async function main(): Promise<void> {
     const langsRaw = (opts.langs || opts.lang || '') as string;
     const langs = langsRaw
         ? langsRaw
-            .split(/[ ,]+/)
-            .map((s) => s.trim())
-            .filter(Boolean)
+              .split(/[ ,]+/)
+              .map((s) => s.trim())
+              .filter(Boolean)
         : [];
 
     const simVersionsRaw = (opts['simulated-winccoa-versions'] || '') as string;
@@ -266,7 +266,7 @@ export async function main(): Promise<void> {
             if (!fs.existsSync(cfgPath)) {
                 const contentLines: string[] = [
                     '[general]',
-                    `pvss_path = "${(oaPath ?? '').replace(/\\/g, '/')}"`
+                    `pvss_path = "${(oaPath ?? '').replace(/\\/g, '/')}"`,
                 ];
 
                 if (opts['sub-project'] !== undefined) {
@@ -310,7 +310,7 @@ export async function main(): Promise<void> {
                         contentLines.push(`proj_path = "${subProjPath.replace(/\\/g, '/')}"`);
                     }
                 }
-                
+
                 contentLines.push(`proj_path = "${absProjectPath.replace(/\\/g, '/')}"`);
                 if (winccVersion) contentLines.push(`proj_version = "${winccVersion}"`);
                 if (langs.length) contentLines.push(`langs = "${langs.join(' ')}"`);
